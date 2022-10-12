b2ce0eb294a1443f2651e5ddfe9760e1d2d8c3c5
ParkMinjeong <minwjd1028@g.skku.edu>
2022-10-12 11:05:24 +0900

    221012_틀 작성
    - html 파일 정리
    - 틀 작성

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -9,9 +9,17 @@ from apps.home import views
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
+    path("", views.index, name="home"),
 
     # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    # re_path(r"^.*\.*", views.pages, name="pages"),
 
+    # 추가한 내용
+    path("support/", views.support_bulletin),
+    path("route/", views.route_bulletin),
+    path("search/", views.search),
+    path("supportWrite/", views.support_write),
+    path("supportView/", views.support_view),
+    path("routeWrite/", views.route_write),
+    path("routeView/", views.route_view),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -4,41 +4,66 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django import template
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.shortcuts import render
 
 
-@login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
 
-    html_template = loader.get_template('home/index.html')
+    html_template = loader.get_template('home/home.html')
     return HttpResponse(html_template.render(context, request))
 
 
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
+def support_bulletin(request):
+    return render(request, "home/support-bulletin.html")
 
-        load_template = request.path.split('/')[-1]
 
-        if load_template == 'admin':
-            return HttpResponseRedirect(reverse('admin:index'))
-        context['segment'] = load_template
+def support_write(request):
+    return render(request, "home/support-write.html")
 
-        html_template = loader.get_template('home/' + load_template)
-        return HttpResponse(html_template.render(context, request))
 
-    except template.TemplateDoesNotExist:
+def support_view(request):
+    return render(request, "home/route-support.html")
 
-        html_template = loader.get_template('home/page-404.html')
-        return HttpResponse(html_template.render(context, request))
 
-    except:
-        html_template = loader.get_template('home/page-500.html')
-        return HttpResponse(html_template.render(context, request))
+def route_bulletin(request):
+    return render(request, "home/route-bulletin.html")
+
+
+def route_write(request):
+    return render(request, "home/route-write.html")
+
+
+def route_view(request):
+    return render(request, "home/route-view.html")
+
+
+def search(request):
+    return render(request, "home/search.html")
+
+
+# def pages(request):
+#     context = {}
+#     # All resource paths end in .html.
+#     # Pick out the html file name from the url. And load that template.
+#     try:
+#         load_template = request.path.split('/')[-1]
+#
+#         if load_template == "admin":
+#             return HttpResponseRedirect(reverse("admin:index"))
+#         context["segment"] = load_template
+#
+#         html_template = loader.get_template("home/" + load_template)
+#         return HttpResponse(html_template.render(context, request))
+#
+#     except template.TemplateDoesNotExist:
+#         html_template = loader.get_template("home/page-404.html")
+#         return HttpResponse(html_template.render(context, request))
+#
+#     except:
+#         html_template = loader.get_template("home/page-500.html")
+#         return HttpResponse(html_template.render(context, request))
